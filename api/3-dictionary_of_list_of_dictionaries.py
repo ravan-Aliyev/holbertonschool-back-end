14cbc68ce9f7d6ec9a16141f0ef3a10fe3a2af33
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-04-22 13:26:55 +0400

    dictionary of list of dictionaries

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Requesting data from rest api
+"""
+from sys import argv
+import json
+import requests
+
+
+if __name__ == "__main__":
+    users = requests.get(
+        f'https://jsonplaceholder.typicode.com/users').json()
+
+    username = {}
+
+    for user in users:
+        username[user['id']] = user['username']
+
+    todos = requests.get(
+        f'https://jsonplaceholder.typicode.com/todos').json()
+
+    with open("todo_all_employees.json", 'w') as file:
+        tasks_by_user = {}
+
+        for new_task in todos:
+            user_id = new_task['userId']
+
+            if user_id not in tasks_by_user:
+                tasks_by_user[user_id] = []
+
+            new_task_title = new_task['title']
+            new_task_status = new_task['completed']
+            user_id = new_task['userId']
+            task_data = {
+                "username": username[user_id],
+                "task": new_task_title,
+                "completed": new_task_status
+                }
+            tasks_by_user[user_id].append(task_data)
+        json.dump(tasks_by_user, file)
