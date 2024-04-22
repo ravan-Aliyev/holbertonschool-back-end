fd4a45cd06c22a0881004608e54038a60ec6cc2e
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-04-22 12:44:13 +0400

    export to csv

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Requesting data from rest api
+"""
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+    if (len(sys.argv) < 2):
+        print("Please enter user id")
+        sys.exit()
+
+    link = 'https://jsonplaceholder.typicode.com'
+
+    user = requests.get('{0}/users/{1}'
+                        .format(link, sys.argv[1])).json()
+    userTodo = requests.get('{0}/todos?{1}={2}'
+                            .format(link, "userId", sys.argv[1])).json()
+
+    csvFormat = []
+
+    for data in userTodo:
+        arr = [f"{data['userId']}", f"{user['name']}",
+               f"{data['completed']}", f"{data['title']}"]
+        csvFormat.append(arr)
+
+    filename = f"{user['id']}.csv"
+
+    with open(filename, 'w', newline="") as file:
+        csvwriter = csv.writer(file, quoting=csv.QUOTE_ALL)
+        csvwriter.writerows(csvFormat)
