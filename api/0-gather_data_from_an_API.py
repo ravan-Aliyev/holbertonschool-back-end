4466919138323c720144f8fdd0c25acdb655056a
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-04-22 11:40:02 +0400

    gather data from api

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if (len(sys.argv) < 2):
+    print("Please enter user id")
+    sys.exit()
+
+dataUser = requests.get('https://jsonplaceholder.typicode.com/users').json()
+dataToDo = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+
+user = next((u for u in dataUser if u['id'] == int(sys.argv[1])), None)
+
+if user is None:
+    print("User not found")
+    sys.exit(1)
+
+usersTodos = [todo for todo in dataToDo if todo['userId'] == user['id']]
+
+done = sum(1 for todo in usersTodos if todo['completed'])
+tasks = len(usersTodos)
+
+print(f"Employee {user['name']} is done with tasks ({done}/{tasks}):")
+for task in usersTodos:
+    if task['completed']:
+        print(f"\t {task['title']}")
+
