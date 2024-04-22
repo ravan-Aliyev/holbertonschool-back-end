f738f0b3c0ccff1f3bf21516bf2ccf1f847e934e
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-04-22 13:14:19 +0400

    export to json

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Requesting data from rest api
+"""
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+    if (len(sys.argv) < 2):
+        print("Please enter user id")
+        sys.exit()
+
+    link = 'https://jsonplaceholder.typicode.com'
+
+    user = requests.get('{0}/users/{1}'
+                        .format(link, sys.argv[1])).json()
+    userTodo = requests.get('{0}/todos?{1}={2}'
+                            .format(link, "userId", sys.argv[1])).json()
+
+    tasksArr = []
+
+    for data in userTodo:
+        obj = {"task": data['title'], "completed": data['completed'],
+               "username": user['username']}
+        tasksArr.append(obj)
+
+    filename = f"{user['id']}.json"
+
+    with open(filename, 'w', encoding="utf-8") as file:
+        serialized = {user['id']: tasksArr}
+        json.dump(serialized, file)
