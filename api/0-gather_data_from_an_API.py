918da9638529957864e320b877bfcd8aa0878725
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-04-22 11:52:45 +0400

    gather data from api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,25 +5,20 @@ Requesting data from rest api
 import requests
 import sys
 
-if (len(sys.argv) < 2):
-    print("Please enter user id")
-    sys.exit()
+if __name__ == "__main__":
+    if (len(sys.argv) < 2):
+        print("Please enter user id")
+        sys.exit()
 
-dataUser = requests.get('https://jsonplaceholder.typicode.com/users').json()
-dataToDo = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    user = requests.get(f'https://jsonplaceholder.typicode.com/users/\
+                        {sys.argv[1]}').json()
+    userTodo = requests.get(f'https://jsonplaceholder.typicode.com/todos\
+                            ?userId={sys.argv[1]}').json()
 
-user = next((u for u in dataUser if u['id'] == int(sys.argv[1])), None)
+    done = sum(1 for todo in userTodo if todo['completed'])
+    tasks = len(userTodo)
 
-if user is None:
-    print("User not found")
-    sys.exit(1)
-
-usersTodos = [todo for todo in dataToDo if todo['userId'] == user['id']]
-
-done = sum(1 for todo in usersTodos if todo['completed'])
-tasks = len(usersTodos)
-
-print(f"Employee {user['name']} is done with tasks ({done}/{tasks}):")
-for task in usersTodos:
-    if task['completed']:
-        print(f"\t {task['title']}")
+    print(f"Employee {user['name']} is done with tasks ({done}/{tasks}):")
+    for task in userTodo:
+        if task['completed']:
+            print(f"\t {task['title']}")
