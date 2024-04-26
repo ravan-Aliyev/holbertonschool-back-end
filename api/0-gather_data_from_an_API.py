c3ba22c5fab3cb70e9e9ce4ce58c668d13aeffc8
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-04-26 16:02:39 +0400

    fixing error

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,27 +1,41 @@
 #!/usr/bin/python3
 """
-Requesting data from rest api
+Python script that, using REST API,
+for a given employee ID,
+returns information about his/her TODO list progress.
 """
+import json
 import requests
-import sys
+from sys import argv
+
+
+def gather_user_information():
+    """
+    Fethces and processes user and TODO list data for the provided ID.
+    """
+    Id = argv[1]
+    user_url = f'https://jsonplaceholder.typicode.com/users/{Id}'
+    tasks_url = f'https://jsonplaceholder.typicode.com/todos?userId={Id}'
+
+    user = json.loads(requests.get(user_url).text)
+    tasks = json.loads(requests.get(tasks_url).text)
+
+    completed_tasks = []
+    completed = 0
+    employee_name = user['name']
+
+    for task in tasks:
+        if task['completed'] is True:
+            completed += 1
+            completed_tasks.append(task['title'])
+
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+                                                          completed,
+                                                          len(tasks)))
+    for title in completed_tasks:
+        print(f"\t {title}")
+
 
 if __name__ == "__main__":
-    if (len(sys.argv) < 2):
-        print("Please enter user id")
-        sys.exit()
-    link = 'https://jsonplaceholder.typicode.com'
-
-    user = requests.get('{0}/users/{1}'
-                        .format(link, sys.argv[1])).json()
-    userTodo = requests.get('{0}/todos?{1}={2}'
-                            .format(link, "userId", sys.argv[1])).json()
-
-    done = sum(1 for todo in userTodo if todo['completed'])
-    tasks = len(userTodo)
-
-    print("Employee {0} is done with tasks ({1}/{2}):"
-          .format(user['name'], done, tasks))
-
-    for task in userTodo:
-        if task['completed']:
-            print(" \t{0}".format(task['title']))
+    if len(argv) == 2:
+        gather_user_information()
